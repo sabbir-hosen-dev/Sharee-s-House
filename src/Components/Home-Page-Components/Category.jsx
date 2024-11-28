d4cb0b14b8139a58682189368a795947fb0322f8
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-11-28 21:18:28 +0600

    push

--- src/Components/Home-Page-Components/Category.jsx
+++ src/Components/Home-Page-Components/Category.jsx
@@ -2,7 +2,7 @@ import categoryData from "../../Data/Category.json";
 function Category() {
   return (
     <section className="wrap">
-      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5">
+      <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-5">
         {categoryData.map((category) => (
           <div key={category.id} className="rounded-md relative">
           <h2 className="font-extrabold rounded-md text-white absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
